edf983bf4afe038c0291f9fee445045abcf6ae0a
masaru.takahashi <masaru.takahashi@scsk.jp>
2021-01-05 23:58:05 +0900

    Fix NatGateway PrivateからPublicSubnetに修正

--- example/multi-private-subnet.tf
+++ example/multi-private-subnet.tf
@@ -51,12 +51,12 @@ resource "aws_route_table_association" "private_1" {
 # # NATGateway
 # resource "aws_nat_gateway" "nat_gateway_0" {
 #   allocation_id = aws_eip.nat_gateway_0.id
-#   subnet_id     = aws_subnet.private_0.id
+#   subnet_id     = aws_subnet.public_0.id
 #   depends_on    = [aws_internet_gateway.example]
 # }
 # resource "aws_nat_gateway" "nat_gateway_1" {
 #   allocation_id = aws_eip.nat_gateway_1.id
-#   subnet_id     = aws_subnet.private_1.id
+#   subnet_id     = aws_subnet.public_1.id
 #   depends_on    = [aws_internet_gateway.example]
 # }
 
